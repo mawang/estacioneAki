dcc553d56759c45704bb1925beee2f8c8a414a76
tenoriobruno <bruno.tenorio1@gmail.com>
2014-05-07 16:26:14 -0300

    Padronização do nome de variaveis para o português. Inserção de getters e setters na Estacionamento.java Leitura e plotagem de estacionamentos a partir de um xml local.

--- estacione_aki_mobile/src/estacioneAki/activities/EstacioneAkiMap.java
+++ estacione_aki_mobile/src/estacioneAki/activities/EstacioneAkiMap.java
@@ -1,11 +1,6 @@
 package estacioneAki.activities;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Iterator;
 
 import org.simpleframework.xml.Serializer;
@@ -15,44 +10,47 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import estacioneAki.util.Estacionamento;
 import estacioneAki.util.EstacionamentoList;
 import android.app.Activity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.util.Log;
-import estacioneAki.util.*;
 
 public class EstacioneAkiMap extends Activity {
 
 	private GoogleMap mMap;
 	
-	void readLocalXML() throws Exception{
+	Iterator<Estacionamento> leXMLLocal() throws Exception{
 		
 		AssetManager assetMan = getAssets();
 		InputStream is = assetMan.open("listaEstacionamento.xml");
 		Serializer serializer = new Persister();
         EstacionamentoList estList = serializer.read(EstacionamentoList.class, is);
         Iterator<Estacionamento> iterList = estList.estaciomentoList.iterator();
-        while(iterList.hasNext()){
-        	Estacionamento e = iterList.next();
-        	Log.v("estacionameto", e.nome+" "+e.cnpj+"\n");
-        }
-	}
-	
-	
+        return iterList;
+ 	}	
 
-    @Override
+	void plotaEstacionamentosNoMapa(Iterator<Estacionamento> iterList){
+	    while(iterList.hasNext()){
+	    	Estacionamento e = (Estacionamento) iterList.next();
+	    	MarkerOptions marker = new MarkerOptions();
+	    	LatLng position = new LatLng(new Double(e.getLatitude()).doubleValue(), new Double(e.getLongitude()).doubleValue());
+	        marker.position(position);
+	        marker.title(e.getNome());
+	        marker.snippet(e.getEndereco()+". Preço/hora: R$"+e.getPrecoHora());
+	        mMap.addMarker(marker);
+	    } 
+	}
+    
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_estacione_aki_map);
-        
-        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Hello world"));
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_estacione_aki_map);
+       mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
        try {
-			readLocalXML();
+    	   //Iterator<Estacionamento> iterList = (Iterator<Estacionamento>) sc.execute();
+    	   Iterator<Estacionamento> iterList = leXMLLocal();
+    	   plotaEstacionamentosNoMapa(iterList);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
